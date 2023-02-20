78d3c0626efa22f32b1dd4a4df7594345980b169
moonjaeseong <wotmdskfk32@naver.com>
2023-02-20 23:44:08 +0900

    옹알이 문제

--- src/옹알이.java
+++ src/옹알이.java
@@ -1,3 +1,17 @@
-public class 옹알이 {
+class Solution {
 
+    public static String[] babblingArray = {"aya", "ye", "woo", "ma"};
+
+    public int solution(String[] babbling) {
+        int answer = 0;
+
+        for(String bab : babbling){
+            for(String str : babblingArray) {
+                bab = bab.replaceFirst(str, " ");
+            }
+            if(bab.replaceAll(" ","").equals("")) answer++;
+        }
+
+        return answer;
+    }
 }
