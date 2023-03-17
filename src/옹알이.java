2a72a9eaa5c359fd41eca4e2d70f620db80552c7
moonjaeseong <wotmdskfk32@naver.com>
2023-03-17 09:00:13 +0900

    수식 변경2

--- src/옹알이.java
+++ src/옹알이.java
@@ -1,6 +1,6 @@
 class Solution {
 
-    public static String[] babblingArray = {"aya", "yes", "woo", "ma"};
+    public static String[] babblingArray = {"ayxa", "yes", "woo", "ma"};
 
     public int solution(String[] babbling) {
         int answer = 0;
